9e4af18db4081d17c16c7c70028f45bd3ed0d131
Brian Wong <brianw385@gmail.com>
2020-03-04 02:25:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -48,6 +48,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array = array.slice(0, array.length);
+  array = array.slice(0, array.length - 1);
   return array
 }
